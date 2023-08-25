5834a412e61656e865ce120340c1d03adb016cda
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-25 14:38:37 +0100

    fix copying the command of files creation and adding animation include logo

--- popup/popup.js
+++ popup/popup.js
@@ -8,28 +8,46 @@ function checkUncheckButton(checkbox) {
 		checkbox.checked = false;
 	}
 }
+function copyToClipboard(text) {
+	navigator.clipboard.writeText(text)
+	  .then(() => {
+		console.log('Text copied to clipboard');
+	  })
+	  .catch((error) => {
+		console.error('Unable to copy text', error);
+	  });
+  }
+
 document.querySelector('#scrape-button').addEventListener('click', () => {
 	browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		browser.tabs.sendMessage(tabs[0].id, { action: 'scrapeFileNames' }, function (response) {
+			copyToClipboard(response["cmd"]);
 			let scrapeButton = document.getElementById('scrape-button');
 			if (response.success) {
 				scrapeButton.classList.remove('error');
-				scrapeButton.textContent = 'Copied to clipboard';
+				scrapeButton.textContent = 'Copied';
+				setTimeout(()=>{
+					scrapeButton.textContent = 'Files';
+				},2000)
 			} else {
 				scrapeButton.classList.add('error');
-				scrapeButton.textContent = 'Error copying to clipboard';
+				scrapeButton.textContent = 'Copying error';
 			}
 		});
 	});
 });
-document.addEventListener('DOMContentLoaded', function () {
+
+
+window.onload = function () {
 	browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		browser.tabs.sendMessage(tabs[0].id, { message: "get_task_status" }, function (response) {
 			let taskStatus = response.taskStatus;
+			let taskIds = response.taskIds;
 			let taskList = document.getElementById('task-list');
 			taskStatus.forEach(function (status, index) {
 				let label = document.createElement('label');
 				label.classList.add('task-status');
+				label.setAttribute('data-task-id', taskIds[index]);
 				if (status === 'yes') {
 					label.classList.add('green');
 				} else if (status === 'no') {
@@ -38,11 +56,11 @@ document.addEventListener('DOMContentLoaded', function () {
 				let checkbox = document.createElement('input');
 				checkbox.type = 'checkbox';
 				label.appendChild(checkbox);
-				label.appendChild(document.createTextNode((index)));
+				if (index <= 9)
+					label.appendChild(document.createTextNode((" Task " + index)));
+				else
+					label.appendChild(document.createTextNode((" Task " + index)));
 				taskList.appendChild(label);
-				const parentLabel = checkbox.parentElement;
-				parentLabel.classList.add('checked');
-				checkbox.checked = true;
 				checkbox.addEventListener('change', () => {
 					checkUncheckButton(checkbox);
 				});
@@ -70,14 +88,20 @@ document.addEventListener('DOMContentLoaded', function () {
 		let checkboxes = document.querySelectorAll('#task-list input[type="checkbox"]');
 		let taskIndices = [];
 		checkboxes.forEach(function (checkbox, index) {
-			if (checkbox.checked) {
+			const parentLabel = checkbox.parentElement;
+			if (checkbox.checked && !parentLabel.classList.contains('running')) {
 				taskIndices.push(index);
+				parentLabel.classList.add('running');
 			}
 		});
 		browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 			browser.tabs.sendMessage(tabs[0].id, { message: "run_script", taskIndices: taskIndices }, function (response) {
 				runScriptButton.textContent = 'Run Script';
+				setTimeout(()=>{
+					runScriptButton.textContent = 'Check';
+				}, 1000)
 			});
+		browser.tabs.sendMessage(tabs[0].id, { message: "create_observers" }, function (response) { });
 		});
 	});
 
@@ -144,4 +168,38 @@ document.addEventListener('DOMContentLoaded', function () {
 			}
 		});
 	});
-});
+	browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.message === 'spinner_hidden') {
+			let hasTaskPassed = false;
+			let taskId = request.taskId;
+			let label = document.querySelector(`label[data-task-id="${taskId}"]`);
+			let checkbox = label.querySelector('input[type="checkbox"]');
+			let studentTaskDoneElement = new DOMParser().parseFromString(request.studentTaskDoneElement, 'text/html').body.firstChild;
+
+			label.classList.remove('running');
+			label.classList.remove('checked');
+			checkbox.checked = false;
+			if (studentTaskDoneElement.classList.contains('yes')) {
+				label.classList.add('green');
+				label.classList.remove('red');
+				hasTaskPassed = true;
+			} else {
+				label.classList.add('red');
+				label.classList.remove('green');
+			}
+
+			// get the textSpan element
+			let textSpan = label.querySelector('.task-index');
+			// update the text of the textSpan element to show temp feedback
+			let originalText = textSpan.textContent;
+			textSpan.textContent = hasTaskPassed ? 'OK' : 'X';
+
+			console.log(`originalText: ${originalText}`); // log the original text
+
+			setTimeout(function () {
+				console.log('setTimeout called'); // log when the setTimeout function is called
+				textSpan.textContent = originalText;
+			}, 1000);
+		}
+	});
+};
--- src/content.js
+++ src/content.js
@@ -1,23 +1,3 @@
-let taskNum = '';
-document.addEventListener('keydown', function (event) {
-	if (event.keyCode >= 48 && event.keyCode <= 57) {
-		if (event.shiftKey && taskNum.length < 2) {
-			taskNum += String.fromCharCode(event.keyCode);
-		} else {
-			taskNum = String.fromCharCode(event.keyCode);
-		}
-		let task = document.querySelector('#task-num-' + taskNum);
-		if (task) {
-			task.scrollIntoView();
-		}
-	}
-});
-
-document.addEventListener('keyup', function (event) {
-	if (event.keyCode === 16) taskNum = '';
-	if (event.keyCode >= 48 && event.keyCode <= 57 && !event.shiftKey) taskNum = '';
-});
-
 browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 	if (msg.action === 'scrapeFileNames') {
 		function scrapeFileNames(html) {
@@ -81,38 +61,17 @@ browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 		let html = document.documentElement.outerHTML;
 		let { dirName, fileNames } = scrapeFileNames(html);
 		let commandLine = generateCommandLine(dirName, fileNames);
-
-		let textarea = document.createElement('textarea');
-		// alert(commandLine);
-		textarea.textContent = commandLine;
-		document.body.appendChild(textarea);
-
-		textarea.select();
-
-		function delay(ms) {
-			return new Promise(resolve => {
-			  setTimeout(resolve, ms);
-			});
-		}
-
-		try {
-			let successful = document.execCommand('copy');
-			let msg = successful ? 'Command line copied to clipboard!' : 'Could not copy command line to clipboard';
-			sendResponse({ success: true })
-		} catch (err) {
-			sendResponse({ success: false })
-			console.error('Could not copy command line to clipboard: ', err);
-		}
-
-		document.body.removeChild(textarea);
+		sendResponse({ success: true ,"cmd": commandLine});
+		return;
 	}
 });
-
+let observers = {};
 browser.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
 		if (request.message === "get_task_status") {
 			let buttons = document.querySelectorAll('.student_task_done');
 			let taskStatus = [];
+			let taskIds = [];
 			buttons.forEach(function (button) {
 				if (button.classList.contains('yes')) {
 					taskStatus.push('yes');
@@ -121,13 +80,63 @@ browser.runtime.onMessage.addListener(
 				} else {
 					taskStatus.push('default');
 				}
+				taskIds.push(button.getAttribute('data-task-id'));
 			});
-			sendResponse({ taskStatus: taskStatus });
+			// sendResponse({ taskStatus: taskStatus });
+			sendResponse({ taskStatus: taskStatus, taskIds: taskIds });
 		} else if (request.message === "run_script") {
 			let taskIndices = request.taskIndices;
 			let buttons = document.querySelectorAll('.correction_request_test_send');
 			taskIndices.forEach(function (index) {
 				buttons[index].click();
+			});
+			sendResponse({ success: true });
+		}
+		//+++++++++++++++++++++++++++++++++++++++++ 
+		else if (request.message === "create_observers") {
+			// get all spinner elements
+			let spinners = document.querySelectorAll('.task_correction_modal .spinner');
+
+			// iterate over each spinner element
+			spinners.forEach(function (spinner) {
+				// get the corresponding task ID using the data-task-id attribute
+				let taskId = spinner.parentElement.querySelector('.correction_request_test_send').getAttribute('data-task-id');
+
+				// check if an observer already exists for this taskId
+				if (observers[taskId]) return;
+
+				// create a MutationObserver to watch for changes in the style attribute of the spinner element
+				let observer = new MutationObserver(function (mutations) {
+					mutations.forEach(function (mutation) {
+						if (mutation.attributeName === 'style') {
+							let displayValue = spinner.style.display;
+							if (displayValue === 'none') {
+
+								// add a delay before querying the student_task_done element
+								setTimeout(function () {
+									// get the corresponding student_task_done element
+									let studentTaskDoneElement = document.querySelector(`.student_task_done[data-task-id="${taskId}"]`);
+
+									// send a message to the popup script to notify it that the spinner has been hidden
+									chrome.runtime.sendMessage({
+										message: 'spinner_hidden',
+										taskId: taskId,
+										studentTaskDoneElement: studentTaskDoneElement.outerHTML
+									});
+								}, 1000);
+
+								// disconnect the observer
+								// observer.disconnect();
+							}
+						}
+					});
+				});
+
+				// start observing the spinner element
+				observer.observe(spinner, { attributes: true });
+
+				// store the observer in the observers object using the task ID as the key
+				observers[taskId] = observer;
 			});
 			sendResponse({ success: true });
 		}
